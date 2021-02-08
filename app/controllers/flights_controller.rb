91c21a5558188191f04205c322a64a348e740e3e
kostas-s <ksmonos@hotmail.com>
2021-02-08 21:04:45 +0200

    Layout and form done, w placeholder data

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,7 @@
 class FlightsController < ApplicationController
   def index
-    @airport_options = Airport.all.map { |airport| ["#{airport.name} [#{airport.acronym}] ", airport.id] }
+    @airport_options = Airport.all.map { |airport| ["[#{airport.acronym}] #{airport.name}", airport.id] }
+    @dates = [Date.new(2021, 2, 10), Date.new(2021, 2, 11), Date.new(2021, 2, 12)]
+    @passengers = (1..4)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Airport.delete_all
 Flight.delete_all
-a1 = Airport.create(name: 'Hartsfield-Jackson', acronym: 'ATL/KATL', location: 'Atlanta, Georgia, US')
-a2 = Airport.create(name: 'Beijing Capital', acronym: 'PEK/ZBAA', location: 'Chaoyung-Shunyi, China')
-a3 = Airport.create(name: 'Los Angeles International Airport', acronym: 'LAX/KLAX', location: 'Los Angeles, California')
+a1 = Airport.create(name: 'John F. Kennedy International Airport', acronym: 'NYC/JFK', location: 'Queens, US')
+a2 = Airport.create(name: 'San Francisco International Airport', acronym: 'SFO', location: 'San Francisco, US')
 
 Flight.create(arrival_airport: a1, departure_airport: a2, date: DateTime.now)
